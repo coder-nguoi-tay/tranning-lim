b17cfe69b8aefb0f5e184039d888a342f2304845
trieuvietduc <tdtazan112@gmail.com>
2023-07-03 08:30:11 +0700

    update dockerfile

--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 Route::prefix('v1')->group(function () {
     Route::post('/login', [LoginController::class, 'login']);
     Route::post('/register', [LoginController::class, 'register']);
